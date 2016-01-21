9999bc29d808902767d4723fa50a2d4a40c7517a
spdcoder <sdppsudheer@gmail.com>
2016-01-20 23:47:56 -0500

    Update Transaction.java

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -3,6 +3,8 @@ package com.abc;
 import java.util.Calendar;
 import java.util.Date;
 
+import static java.lang.Math.abs;
+
 public class Transaction {
     public final double amount;
 
@@ -11,6 +13,34 @@ public class Transaction {
     public Transaction(double amount) {
         this.amount = amount;
         this.transactionDate = DateProvider.getInstance().now();
+    }
+
+    public Transaction(double amount, Date date) {
+        this.amount = amount;
+        this.transactionDate = date;
+    }
+
+    public String printTransaction() {
+        return (amount < 0 ? "withdrawal" : "deposit") + " " + PrintHelper.toDollars(amount);
+    }
+
+    public double getAmount()
+    {
+        return amount;
+    }
+
+    public Date getTransactionDate()
+    {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(Date transactionDate)
+    {
+        this.transactionDate = transactionDate;
+    }
+
+    public boolean isDeposit() {
+        return this.amount > 0;
     }
 
 }
