dd865482e8f5feb31991cfcf2a9e783d96d917a0
spdcoder <sdppsudheer@gmail.com>
2016-01-20 23:51:15 -0500

    Update TransactionTest.java

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
 public class TransactionTest {
-    @Test
+    @test
     public void transaction() {
         Transaction t = new Transaction(5);
         assertTrue(t instanceof Transaction);
